c583a0e7a1ce4a7693572c5d0c0a53c4c499602c
LIzardLoser <46665722+LIzardLoser@users.noreply.github.com>
2026-06-16 20:23:20 -0400

    tes

--- ShakeButton/NewPagescript.js
+++
@@ -1,7 +0,0 @@
-const elm = document.getElementById('ShakeButton');
-
-elm.onclick = function() {
-    elm.classList.remove('shakeactive');
-    void elm.offsetWidth;
-    elm.classList.add('shakeactive');
-}
